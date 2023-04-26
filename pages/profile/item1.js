a3bd3ff8408929f1e47a8496d6e110edabd6d9d1
Emmanuel Okon <emmaudo27@gmail.com>
2023-04-26 21:15:15 +0100

    bug fixes

--- pages/profile/item1.js
+++ pages/profile/item1.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import SecondChart from "../../components/Second.js";
 export default function Item1() {
   <>
     <Head>
@@ -10,7 +9,7 @@ export default function Item1() {
       <div className="flex flex-col">
         <div className="my-2 overflow-x-auto sm:mx-6 lg:mx-8">
           <div className="py-2 align-middle inline-block min-w-full px-2">
-            <SecondChart />
+            This is Item 2
           </div>
         </div>
       </div>
